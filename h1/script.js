68755aed87c99a6ece203f04cd9696cc23382aa8
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2019-12-07 08:36:10 +0100

    h1

---
+++ h1/script.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+
+for(var i = 0; i < 10; i++)
+	console.log("de waarde van i is " + i)
+
+var mijnauto = {
+	merk: "ford",
+	type: "mondeo",
+	aantalwielen: 4,
+	kleur: "blauw"
+}
+
+console.log (mijnauto.kleur)
