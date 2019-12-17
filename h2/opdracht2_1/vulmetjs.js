092d7dc6f863d591b704ccd9e31bfe12155c2926
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2019-12-17 13:28:36 +0100

    Add files via upload

---
+++ h2/opdracht2_1/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById('paginatitel').innerText = "mijn geweldige pagina";
+document.getElementById('subkop').innerText = "mijn geweldige subkop";
+document.getElementById('aap1').src = "img/aap1.png";
+document.getElementById('aap2').src = "img/aap2.png";
---
+++ h2/opdracht2_2/script.js
@@ -0,0 +1,25 @@
+// color
+document.getElementById("bl1").style.backgroundColor = "#3fd1d9";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+//heights
+document.getElementById("bl1").style.height = "100px";
+document.getElementById("bl2").style.height = "100px";
+document.getElementById("bl3").style.height = "100px";
+document.getElementById("bl4").style.height = "100px";
+//width
+document.getElementById("bl1").style.width = "100px";
+document.getElementById("bl2").style.width = "100px";
+document.getElementById("bl3").style.width = "100px";
+document.getElementById("bl4").style.width = "100px";
+//margin top
+document.getElementById("bl1").style.marginTop = "0px";
+document.getElementById("bl2").style.marginTop = "15px";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl4").style.marginTop = "0px";
+//text align
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl4").style.textAlign = "left";
